4a0fe777f97639b4bfa6342d1b138ed44acc1f3f
Aman Kharbanda <amankharbanda2002@gmail.com>
2024-04-21 13:53:41 +0530

    frontend

---
+++ frontend/src/components/Job/Jobs.jsx
@@ -0,0 +1,30 @@
+import React, { useContext, useEffect, useState } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import { Context } from "../../main";
+
+const Jobs = () => {
+
+  return (
+    <section className="jobs page">
+      <div className="container">
+        <h1>ALL AVAILABLE JOBS</h1>
+        <div className="banner">
+          {jobs.jobs &&
+            jobs.jobs.map((element) => {
+              return (
+                <div className="card" key={element._id}>
+                  <p>{element.title}</p>
+                  <p>{element.category}</p>
+                  <p>{element.country}</p>
+                  <Link to={`/job/${element._id}`}>Job Details</Link>
+                </div>
+              );
+            })}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Jobs;
